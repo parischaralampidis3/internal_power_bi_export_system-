e3f416da6093d90e3340f832020bf34f62faec16
Paris Charalampidis <parou.char@gmail.com>
2025-12-19 11:57:56 +0200

    apply changes

--- fast_api/main.py
+++ fast_api/main.py
@@ -1,6 +1,9 @@
 from fastapi import FastAPI
+from routers.routes import router as reports_router
 
 app = FastAPI()
+app.include_router(reports_router, prefix="/api")
+
 @app.get("/")
 def read_root():
     return {"Hello": "World"}
